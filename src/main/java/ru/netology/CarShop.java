cf6441e3edbab712dd453b7f9860d07d2aab6695
Aleksey Alekseev <a.alekseev5743@yandex.ru>
2021-05-20 21:27:44 +0300

    last commit

--- src/main/java/ru/netology/CarShop.java
+++ src/main/java/ru/netology/CarShop.java
@@ -5,8 +5,9 @@ import java.util.List;
 
 public class CarShop {
 
-    private final int ARRIVAL_TIME = 7000;
-    private final int SELL_TIME = 4000;
+    private final int ARRIVAL_TIME = 5000;
+    private final int SEARCH_TIME = 3000;
+    private final int SELL_TIME = 1000;
 
     private final List<Car> cars = new ArrayList<>();
 
@@ -21,22 +22,23 @@ public class CarShop {
             Thread.sleep(ARRIVAL_TIME);
             getCars().add(new Car("BMW"));
             System.out.println("Автосалон: Автомобиль доступен к продаже");
-            notifyAll();
+            notify();
         } catch (InterruptedException err) {
             err.printStackTrace();
         }
     }
 
-    public synchronized Car sellCar () {
+    public synchronized Car sellCar() {
         try {
-            System.out.println("Автосалон: Подбираем нужный автомобиль...");
+            System.out.println(Thread.currentThread().getName() + " зашел в автосалон");
+            Thread.sleep(SEARCH_TIME);
             while (getCars().size() == 0) {
                 System.out.println("Автосалон: Запрошенного автомобиля нет в наличии");
                 wait();
             }
             Thread.sleep(SELL_TIME);
-            System.out.println("Автосалон: Покупатель уехал на новеньком автомобиле");
-         } catch (InterruptedException err) {
+            System.out.println(Thread.currentThread().getName() + " уехал на новеньком автомобиле");
+        } catch (InterruptedException err) {
             err.printStackTrace();
         }
         return getCars().remove(0);
--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -3,31 +3,19 @@ package ru.netology;
 public class Main {
     public static void main(String[] args) throws IllegalThreadStateException {
 
-
-        Customer customer = new Customer();
         CarShop carShop = new CarShop();
-        ThreadGroup customerGroup = new ThreadGroup("customerGroup");
 
-        Thread customer1 = new Thread(customerGroup, customer, "Покупатель1");
-        Thread customer2 = new Thread(customerGroup, customer, "Покупатель2");
-        Thread customer3 = new Thread(customerGroup, customer, "Покупатель3");
-
-        Thread carShopSell = new Thread(null, carShop::sellCar, "Автосалон");
-        Thread carShopGet = new Thread(null, carShop::getCar, "Автосалон");
+        Thread carShopSell1 = new Thread(null, carShop::sellCar, "Покупатель1");
+        Thread carShopSell2 = new Thread(null, carShop::sellCar, "Покупатель2");
+        Thread carShopSell3 = new Thread(null, carShop::sellCar, "Покупатель3");
+        Thread carShopGet = new Thread(null, carShop::getCar);
 
 
-        /**
-         * Покупатели
-         */
-        customer1.start();
-        customer2.start();
-        customer3.start();
-
-        /**
-         * Автосалон
-         */
-        carShopSell.start();
+        carShopSell1.start();
+        carShopSell2.start();
+        carShopSell3.start();
         carShopGet.start();
+
 
     }
 }
