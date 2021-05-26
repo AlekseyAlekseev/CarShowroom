b967b45391822651b5a75a5d88abc5a1b25e11e8
Aleksey Alekseev <a.alekseev5743@yandex.ru>
2021-05-26 17:06:08 +0300

    commit 26.05.21

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -3,22 +3,22 @@ package ru.netology;
 public class Main {
     public static void main(String[] args) throws IllegalThreadStateException {
 
-        final CarShowroom carShowroom = new CarShowroom();
+
+        Producer producer = new Producer();
+        CarShowroom carShowroom = new CarShowroom(producer);
         ThreadGroup customerGroup = new ThreadGroup("customerGroup");
 
         Runnable shopping = carShowroom::carSale;
-        Runnable carProduction = carShowroom::gettingCar;
+        Runnable carProduction = producer::carRelease;
 
         Thread customer1 = new Thread(customerGroup, shopping, "Покупатель1");
         Thread customer2 = new Thread(customerGroup, shopping, "Покупатель2");
         Thread customer3 = new Thread(customerGroup, shopping, "Покупатель3");
         Thread factory1 = new Thread(null, carProduction, "BMW");
-        Thread factory2 = new Thread(null, carProduction, "Mercedes");
 
         customer1.start();
         customer2.start();
         customer3.start();
         factory1.start();
-        factory2.start();
     }
 }
