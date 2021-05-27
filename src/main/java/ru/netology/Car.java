ae7d6ada71de11ee0b2c6b933d5ac73f7d495a82
Aleksey Alekseev <a.alekseev5743@yandex.ru>
2021-05-27 17:00:27 +0300

    commit 27.05.21

--- src/main/java/ru/netology/Car.java
+++ src/main/java/ru/netology/Car.java
@@ -1,35 +1,9 @@
 package ru.netology;
 
-import java.util.ArrayList;
-import java.util.List;
 
-public class Car implements Runnable {
-    public Car () {
-    }
+public class Car {
 
-    // План продаж
-    private final int SALES_PLAN = 10;
-
-    // Время на создание авто
-    private final int CREATE_AUTO = 1000;
-
-    List<Car> cars = new ArrayList<>();
-
-
-    public List<Car> getCars() {
-        return cars;
-    }
-
-    @Override
-    public void run() {
-        try {
-            while (getCars().size() < SALES_PLAN) {
-                Thread.sleep(CREATE_AUTO);
-                getCars().add(new Car());
-                System.out.println("Производитель " + Thread.currentThread().getName() + " выпустил 1 авто");
-            }
-        } catch (InterruptedException err) {
-            err.printStackTrace();
-        }
+    public Car() {
     }
 }
+
